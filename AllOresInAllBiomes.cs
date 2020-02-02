2edb0f078271960086bbd54e668bac9668305d88
Gamer_Z <github@gz0.nl>
2020-02-02 21:34:36 +0100

    Initial commit

---
+++ AllOresInAllBiomes.cs
@@ -0,0 +1,55 @@
+﻿using static ModLoader;
+using TerrainGeneration;
+
+namespace grasmanek94.AllOresInAllBiomes
+{
+    [ModManager]
+    public static class AllOresInAllBiomes
+    {
+        [ModCallback(EModCallbackType.OnAssemblyLoaded, "OnAssemblyLoaded")]
+        static void OnLoad(string assemblyPath)
+        {
+        
+        }
+
+        [ModCallback(EModCallbackType.OnLoadingTerrainGenerator, "OnLoadingTerrainGenerator")]
+        static void OnLoadingTerrainGenerator(TerrainGeneratorBase generatorBase)
+        {
+			if(TerrainModManager.QueuedOreLayers == null)
+			{
+				return;
+			}
+
+            for (int i = 0; i < TerrainModManager.QueuedOreLayers.Length; i++)
+            {
+                TerrainModManager.QueuedOreLayers[i].ScienceBiome = null;
+            }
+
+
+			TerrainGenerator terrainGenerator = generatorBase as TerrainGenerator;
+			TerrainGenerator.IFinalChunkModifier finalChunkModifier = (terrainGenerator != null) ? terrainGenerator.FinalChunkModifier : null;
+			TerrainGenerator.OreLayersGenerator oreLayersGenerator = null;
+			while (finalChunkModifier != null)
+			{
+				oreLayersGenerator = (finalChunkModifier as TerrainGenerator.OreLayersGenerator);
+				if (oreLayersGenerator != null)
+				{
+					break;
+				}
+				finalChunkModifier = finalChunkModifier.InnerGenerator;
+			}
+
+			if (oreLayersGenerator == null)
+			{
+				return;
+			}
+
+			oreLayersGenerator.Layers.Clear();
+
+			for (int i = 0; i < TerrainModManager.QueuedOreLayers.Length; i++)
+			{
+				oreLayersGenerator.AddLayer(TerrainModManager.QueuedOreLayers[i], i == TerrainModManager.QueuedOreLayers.Length - 1);
+			}
+		}
+    }
+}
